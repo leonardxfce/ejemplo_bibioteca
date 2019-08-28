1b2d4a02314afa7f07546e00245475281289072a
leonardoa-eb <leonardoa@eventbrite.com>
2019-08-28 16:16:00 -0300

    add heroku configs

---
+++ src/main/java/inicio/FrontController.java
@@ -0,0 +1,23 @@
+package inicio;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FrontController extends HttpServlet {
+
+    private String message;
+
+    public void init() {
+        message = "Hello World";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>PEPE</h1>");
+    }
+
+}
