7c3d9d7754a9984915038e50da3dc20a8de508d6
leonardoa-eb <leonardoa@eventbrite.com>
2019-08-28 16:24:17 -0300

    add shorcut runner

--- src/main/java/inicio/FrontController.java
+++ src/main/java/inicio/FrontController.java
@@ -17,7 +17,7 @@ public class FrontController extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<h1>PEPE</h1>");
+        out.println("<h1>Leonardo Araoz</h1>");
     }
 
 }
